01f767c15e2323450406eb7795a5af58ce0395ab
Josh Chan <jchan91913@gmail.com>
2017-03-02 15:32:30 -0800

    Initial Commit

---
+++ OTRTables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OTRTables
+//
+//  Created by Josh Chan on 2017-03-02.
+//  Copyright © 2017 Josh Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
